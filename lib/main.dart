c4aa5ca2f77814d4530e26ce9088c6669166ceb1
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-04-08 00:43:09 +0800

    fsf

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,225 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  await Hive.initFlutter();
+  var box = await Hive.openBox('database');
+
+  runApp(const CupertinoApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<MyApp> createState() => _MyAppState();
+} 
+
+class _MyAppState extends State<MyApp> {
+  List<dynamic> todolist = [];
+  List<dynamic> filteredList = [];
+  TextEditingController _addTask = TextEditingController();
+  var box = Hive.box('database');
+
+  String filterMode = "all";
+  ScrollController _scrollController = ScrollController(); // Scroll controller
+
+  @override
+  void initState() {
+    super.initState();
+    try {
+      todolist = box.get('todo') ?? [];
+      todolist = todolist.map((item) {
+        return {
+          "task": item["task"],
+          "status": item["status"] ?? false,
+          "pinned": item["pinned"] ?? false,
+          "checklist": item["checklist"] ?? false,
+          "createdAt": item["createdAt"] ?? DateTime.now(),
+        };
+      }).toList();
+      filteredList = List.from(todolist);
+    } catch (e) {
+      todolist = [];
+      filteredList = [];
+    }
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void applyFilter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (filterMode == "pinned") {
+        filteredList = todolist.where((item) => item['pinned'] == true).toList();
+      } else if (filterMode == "checklist") {
+        filteredList = todolist.where((item) => item['checklist'] == true).toList();
+      } else {
+        filteredList = List.from(todolist);
+      }
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  String formatDateTime(DateTime createdAt) {
+    return "${DateFormat('MMM d').format(createdAt)} at ${DateFormat('HH:mm').format(createdAt)}";
+  }
+
+  Widget _buildHeader(BuildContext context) {
+    final isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
+    final headerColor = isDarkMode ? CupertinoColors.white : CupertinoColors.label;
+
+    if (filterMode == "pinned") {
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: Text(
+          "Pinned Notes",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: headerColor,
+          ),
         ),
+      );
+    } else if (filterMode == "checklist") {
+      return Padding(
+        padding: const EdgeInsets.only(bottom: 5),
+        child: Text(
+          "Checklist Items",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: headerColor,
+          ),
+        ),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  Widget _buildDivider(BuildContext context) {
+    final isDarkMode = MediaQuery.of(context).platformBrightness == Brightness.dark;
+    final dividerColor = isDarkMode ? CupertinoColors.white.withOpacity(0.1) : CupertinoColors.separator;
+
+    if (filterMode == "pinned" || filterMode == "checklist") {
+      return Divider(
+        color: dividerColor,
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  Future<void> _updateTaskDialog(Map<String, dynamic> item) async {
+    TextEditingController _taskController = TextEditingController(text: item['task']);
+    await showCupertinoDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: Text("Update Note"),
+        content: Padding(
+          padding: const EdgeInsets.only(top: 20),
+          child: CupertinoTextField(
+            controller: _taskController,
+            placeholder: 'Update Note',
+          ),
+        ),
+        actions: [
+          CupertinoDialogAction(
+            child: Text("Cancel"),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          CupertinoDialogAction(
+            child: Text("Save"),
+            onPressed: () {
+              setState(() {
+                item['task'] = _taskController.text;
+                int mainIndex = todolist.indexWhere((task) => task['task'] == item['task']);
+                if (mainIndex != -1) todolist[mainIndex]['task'] = item['task'];
+                box.put('todo', todolist);
+              });
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final isSmallScreen = screenWidth < 350;
+
+    double titleFontSize = isSmallScreen ? 14 : 18;
+    double taskFontSize = isSmallScreen ? 12 : 16;
+    double dateFontSize = isSmallScreen ? 10 : 12;
+
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(top: 15),
+          child: Text(
+            "Notes",
+            style: TextStyle(
+              color: CupertinoColors.systemYellow,
+              fontWeight: FontWeight.w600,
+              fontSize: titleFontSize,
+            ),
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CupertinoButton(
+              child: const Icon(
+                CupertinoIcons.settings,
+                color: CupertinoColors.systemYellow,
+                size: 20,
+              ),
+              onPressed: () {
+                showCupertinoDialog(
+                  context: context,
+                  builder: (context) {
+                    return CupertinoAlertDialog(
+                      title: const Text("List of Members"),
+                      content: Column(
+                        children: [
+                          const SizedBox(height: 15),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: Row(
+                              children: [
+                                const SizedBox(height: 15),
+                                Flexible( // Added Flexible
+                                  child: Row(
+                                    children: [
+                                      ClipOval(
+                                        child: Image.asset(
+                                          'images/maryjoyce.jpg',
+                                          height: 50,
+                                          width: 50,
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                      const SizedBox(width: 10),
+                                      Flexible( // Added Flexible
+                                        child: const Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text("Artienda, Mary Joyce", overflow: TextOverflow.ellipsis,),
+                                            Text(
+                                              "UX designer",
+                                              style: TextStyle(color: CupertinoColors.systemGrey2),
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
